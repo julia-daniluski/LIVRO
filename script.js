48d78a28811bc622aa9e38709a07b1359a7f7a0b
julia-daniluski <julia.o.daniluski@gmail.com>
2025-02-24 10:43:17 -0300

    pronto

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-/*
-() const myFunction =()=>{
-       window.alert("você clicou")
-} 
-       */
-      
-let lista = document.querySelectorAll(".item");  //Seleciona todos os elementos com a classe "item"
-//console.log(lista);
+let lista = document.querySelectorAll('.item')
 
-let next = document.getElementById("proximo");  //Seleciona o botão de próximo
-let prev = document.getElementById("anterior");  //Seleciona o botão de anterior
+let next = document.getElementById('proximo')
+let prev = document.getElementById('anterior')
 
-let contar = lista.length;  //Conta os elementos da lista
-let ativo = 0; // Índice do item ativo
+let contar = lista.length
+let ativo = 0
 
-//console.log(contar)
 next.onclick=()=>{
-    console.log('proximo')
     let desativar = document.querySelector('.ativo');
     desativar.classList.remove('ativo');
     if(ativo>=contar-1){
@@ -26,16 +17,13 @@ next.onclick=()=>{
     lista[ativo].classList.add('ativo');
 }
 
-
-prev.onclick =()=>{
-    let desativar = document.querySelector ('.ativo')
-    desativar.classList.remove('ativo')
-    ativo=ativo-1
-    if(ativo>=contar-1){
-        ativo=0;
-    }else{
-        ativo=ativo+1
+prev.onclick = () => {
+    let desativar = document.querySelector('.ativo');
+    desativar.classList.remove('ativo');
+    if (ativo <= 0) {
+        ativo = contar - 1; 
+    } else {
+        ativo = ativo - 1;
     }
-    lista[ativo].classList.add ('ativo')
-}
-
+    lista[ativo].classList.add('ativo');
+};
